b2c7cd553d4858a03798800a3bfa6df586e1e826
Ana Mari <anitamari.1979@gmail.com>
2023-05-06 12:13:40 +0200

    Initial Express commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const cors = require("cors"); //I added this at the top
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(cors()); //I added this after app is created
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public'))); I commented this out
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
